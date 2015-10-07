25de54974f3659ba9dd62f2416fa69d4dc111818
Olivier <olivier.caillier@gmail.com>
2015-10-07 11:14:21 +0200

    test
    commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ATTAC Opale 62</title>
+<link href="/css/base.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<main>
+<header><img src="img/logo.png" width="60" height="115" alt="ATTAC France"/>
+  <h1>ATTAC OPALE 62</h1><img class="imgd" src="img/logo.png" width="60" height="115" alt="ATTAC France"/></header>
+<article>
+  <p>Le site est actuellement hors service..</p>
+  <p>Vous pouvez vous rendre sur le site nationnal d'<a href="https://france.attac.org/">ATTAC France</a>.</p>
+</article>
+</main>
+</body>
+</html>
